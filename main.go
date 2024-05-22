857f1097e8759d5de01ca89a7d1cea1acd243f42
André Rodi <andre.rodi@kaspar-schulz.de>
2024-05-22 08:24:29 +0200

    Works for localhost

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Running...")
+	fmt.Println("Starting server at port 3333...")
+
+	http.HandleFunc("/", landingPageHandler)
+
+	http.ListenAndServe(":3333", nil)
 }
 
+func landingPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
