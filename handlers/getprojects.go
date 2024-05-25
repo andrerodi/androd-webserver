4dce3ef90ff1791cda7d3b7028d5910ede3b4ef7
andrerodi <89607119+andrerodi@users.noreply.github.com>
2024-05-25 22:39:59 +0200

    Added paging based on projects folder

--- handlers/getprojects.go
+++ handlers/getprojects.go
@@ -25,9 +25,11 @@ func (h *ProjectsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		id = 1
 	}
 
+	id = normalizeId(id)
+
 	log.Println("ID:", id)
 
-	templ := templates.Projects(id)
+	templ := templates.Projects(id, GetProjectsCount(), getProjectById(id))
 	templates.Layout(templ, "projects").Render(r.Context(), w)
 }
 
@@ -49,4 +51,50 @@ func GetProjectsCount() int {
 	log.Println("Projects count:", len(files))
 
 	return len(files)
+}
+
+func getProjectById(id int) templates.Project {
+	dir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	projectsDir := filepath.Join(dir, "static", "projects")
+
+	files, err := os.ReadDir(projectsDir)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id = normalizeId(id)
+
+	file := files[id-1]
+
+	log.Println("Project ID:", id, "Name:", file.Name())
+
+	contents, err := os.ReadFile(filepath.Join(projectsDir, file.Name()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return templates.Project{
+		Id:      id,
+		Title:   file.Name(),
+		Content: string(contents),
+	}
+}
+
+func normalizeId(id int) int {
+	if id < 1 {
+		id = 1
+	}
+
+	if id > GetProjectsCount() {
+		id = GetProjectsCount()
+	}
+
+	return id
 }
--- templates/projects_templ.go
+++ templates/projects_templ.go
@@ -11,10 +11,17 @@ import "io"
 import "bytes"
 
 import (
+	"fmt"
 	"strconv"
 )
 
-func Projects(id int) templ.Component {
+type Project struct {
+	Id      int
+	Title   string
+	Content string
+}
+
+func Projects(current, total int, project Project) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -27,20 +34,58 @@ func Projects(id int) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- PAGING LEFT --><a href=\"projects?id={int}\" class=\"button is-danger is-outlined is-small\"><span class=\"icon is-small\"><i class=\"fa fa-power-off\"></i></span></a><div><h1>Projects</h1><p>Project ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- PAGING LEFT --><a href=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(id))
-		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `projects.templ`, Line: 17, Col: 33}
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
+		var templ_7745c5c3_Var2 templ.SafeURL = templ.URL(fmt.Sprintf("projects?id=%s", strconv.Itoa(current-1)))
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(string(templ_7745c5c3_Var2)))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</p></div><!-- PAGING RIGHT --><a href=\"some/link\" class=\"button is-danger is-outlined is-small\"><span class=\"icon is-small\"><i class=\"fa fa-power-off\"></i></span></a>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if current <= 1 {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" disabled style=\"cursor: default; pointer-events: none;\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" class=\"button is-small mr-5\"><span>&#60;&#61;</span></a><div><h1>Projects</h1><p>Project ")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var3 string
+		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(current))
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `projects.templ`, Line: 22, Col: 38}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</p></div><!-- PAGING RIGHT --><a href=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 templ.SafeURL = templ.URL(fmt.Sprintf("projects?id=%s", strconv.Itoa(current+1)))
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(string(templ_7745c5c3_Var4)))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if current >= total {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" disabled style=\"cursor: default; pointer-events: none;\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" class=\"button is-small ml-5\"><span>&#62;&#61;</span></a>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
