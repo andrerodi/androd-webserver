adc4da7d7e7d8995f0ed7b1306655b63cd0322ac
Andre Rodi <andre.rodi@gmail.com>
2024-06-11 22:51:22 +0200

    SPC player wasnt being handled well... maybe we have to rewrite that bitch

--- handlers/getplayground.go
+++ handlers/getplayground.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"androd/templates"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -27,10 +28,21 @@ func (h *echoPlaygroundHandler) ServeHTTP(c echo.Context) error {
 	return nil
 }
 
+func (h *echoPlaygroundHandler) GetSpc(c echo.Context) error {
+	file, err := os.ReadFile("./static/files/hoth.spc")
+	if err != nil {
+		return err
+	}
+
+	c.Response().Writer.Write(file)
+
+	return nil
+}
+
 // Handle all routes for the playground page.
 // this *echo.Echo is a pointer to the echo instance.
 // Extend our type by echo.Echo.
-
 func (e *AggregateHandler) Playground() {
 	e.Echo.GET("/playground", newEchoPlaygroundHandler().ServeHTTP)
+	e.Echo.GET("/playground/spc", newEchoPlaygroundHandler().GetSpc)
 }
